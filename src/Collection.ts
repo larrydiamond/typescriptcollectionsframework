341d654a16989eeff6f5a74a373a471ede2a5625
Larry Diamond <ldiamond@ldiamond.com>
2017-07-16 21:50:28 -0400

    Update Collection.ts

--- src/Collection.ts
+++ src/Collection.ts
@@ -1,3 +1,11 @@
+/**
+ * @license
+ * Copyright Larry Diamond 2017 All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/larrydiamond/typescriptcollectionsframework/LICENSE
+ */
+
 export interface Collection<T extends Collectable> {
   /*
   Modified from https://docs.oracle.com/javase/8/docs/api/java/util/Collection.html
