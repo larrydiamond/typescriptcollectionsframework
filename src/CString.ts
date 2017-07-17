ef4e873db4390758c2b9d86ff052a8e5cf0d6aef
Larry Diamond <ldiamond@ldiamond.com>
2017-07-16 21:50:04 -0400

    Update CString.ts

--- src/CString.ts
+++ src/CString.ts
@@ -1,3 +1,11 @@
+/**
+ * @license
+ * Copyright Larry Diamond 2017 All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/larrydiamond/typescriptcollectionsframework/LICENSE
+ */
+
 export class CString implements Collectable {
   str:string;
   constructor (istr:string) {
