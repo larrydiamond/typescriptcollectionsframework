dfd5dab11c73eb3611d328489e1f9504480ee32b
Larry Diamond <ldiamond@ldiamond.com>
2017-07-16 21:37:20 -0400

    Create List.ts

---
+++ src/List.ts
@@ -0,0 +1,22 @@
+export interface List<T extends Collectable> extends Collection<T> {
+  /*
+  Modified from https://docs.oracle.com/javase/8/docs/api/java/util/List.html
+  Returns the element at the specified position in this list.
+  Parameters:
+  index - index of the element to return
+  Returns:
+  the element at the specified position in this list
+  */
+  get(index:number):T;
+
+  /*
+  Modified from https://docs.oracle.com/javase/8/docs/api/java/util/List.html
+  Replaces the element at the specified position in this list with the specified element (optional operation).
+  Parameters:
+  index - index of the element to replace
+  element - element to be stored at the specified position
+  Returns:
+  the element previously at the specified position
+  */
+  set(index:number, element:T):T;
+}
