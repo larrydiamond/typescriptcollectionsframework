54a68a00bf1bd0f76237ad09dc584ca64b94aeaa
Larry Diamond <ldiamond@ldiamond.com>
2017-07-16 21:49:39 -0400

    Update ArrayList.ts

--- src/ArrayList.ts
+++ src/ArrayList.ts
@@ -1,3 +1,11 @@
+/**
+ * @license
+ * Copyright Larry Diamond 2017 All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/larrydiamond/typescriptcollectionsframework/LICENSE
+ */
+
 export class ArrayList<T extends Collectable> implements List<T> {
   elements:T[] = null;
   sizeValue:number = 0;
