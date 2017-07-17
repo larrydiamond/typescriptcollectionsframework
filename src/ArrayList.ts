6ecaa38f901ec930bb039c75646b32c0e736cab9
Larry Diamond <ldiamond@ldiamond.com>
2017-07-16 21:37:40 -0400

    Create ArrayList.ts

---
+++ src/ArrayList.ts
@@ -0,0 +1,55 @@
+export class ArrayList<T extends Collectable> implements List<T> {
+  elements:T[] = null;
+  sizeValue:number = 0;
+
+  add (t:T) : boolean {
+    if (this.elements == null) {
+      this.elements = new Array<T>();
+    }
+    this.elements.push (t);
+    this.sizeValue = this.sizeValue + 1;
+    return true;
+  }
+
+  clear () {
+    this.elements = new Array<T>();
+    this.sizeValue = 0;
+  }
+
+  get(index:number):T {
+    return this.elements [index];
+  }
+
+  indexOf (t:T) : number {
+    if (this.elements == null)
+      return -1;
+    if (this.sizeValue <= 0)
+      return -1;
+
+    for (let loop:number = 0; loop < this.sizeValue; loop++) {
+      let e = this.get (loop);
+      if (e.equals (t))
+        return loop;
+    }
+
+    return -1;
+  }
+
+  isEmpty () : boolean {
+    if (this.sizeValue == 0)
+      return true;
+
+    return false;
+  }
+
+  set(index:number, element:T):T {
+    let tmp:T = this.elements [index];
+    this.elements [index] = element;
+    return tmp;
+  }
+
+  size () : number {
+    return this.sizeValue;
+  }
+
+}
