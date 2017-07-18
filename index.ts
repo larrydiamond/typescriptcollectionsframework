edb83f470b047f1f25bfa925f50945ee1dcfcc2f
Larry Diamond <ldiamond@ldiamond.com>
2017-07-17 22:38:25 -0400

    Trying to make a build process

---
+++ index.ts
@@ -0,0 +1,6 @@
+export {ArrayList} from "./src/ArrayList";
+export {CNumber} from "./src/CNumber";
+export {Collectable} from "./src/Collectable";
+export {Collection} from "./src/Collection";
+export {CString} from "./src/CString";
+export {List} from "./src/List";
