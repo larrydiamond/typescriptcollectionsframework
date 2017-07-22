d7368ead4c523c67a1b9e6f773b90c42833505e6
Larry Diamond <ldiamond@ldiamond.com>
2017-07-21 23:36:29 -0400

    fix(NaiveMap) getting the imports right for the interface heirarchy

--- definitions/src/NavigableMap.d.ts
+++ definitions/src/NavigableMap.d.ts
@@ -1,2 +1,10 @@
-export interface NavigableMap<K, V> {
+/**
+ * @license
+ * Copyright Larry Diamond 2017 All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/larrydiamond/typescriptcollectionsframework/LICENSE
+ */
+import { Map } from "./Map";
+export interface NavigableMap<K, V> extends Map<K, V> {
 }
