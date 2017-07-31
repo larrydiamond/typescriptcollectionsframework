fdd5940cdee0793c875bf45d2ce3abb8fab3ffbf
Larry Diamond <ldiamond@ldiamond.com>
2017-07-30 21:37:28 -0400

    Create karma.conf.js

---
+++ karma.conf.js
@@ -0,0 +1,15 @@
+module.exports = function(config) {
+    config.set({
+        frameworks: ["jasmine", "karma-typescript"],
+        files: [
+            { pattern: "src/**/*.ts" },
+            { pattern: "spec/**/*.ts" }
+        ],
+        preprocessors: {
+            "src/**/*.ts": ["karma-typescript", "coverage"],
+            "spec/**/*.ts": ["karma-typescript"]
+        },
+        reporters: ["progress", "karma-typescript"],
+        browsers: ["Chrome"]
+    });
+};
