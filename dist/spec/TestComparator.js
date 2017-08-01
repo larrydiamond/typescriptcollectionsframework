783698d63148399c81a7b689dfeb0f05ac2d1ff8
Larry Diamond <ldiamond@ldiamond.com>
2017-07-31 21:09:29 -0400

    Improving code coverage

---
+++ dist/spec/TestComparator.js
@@ -0,0 +1,16 @@
+"use strict";
+/**
+* @license
+* Copyright Larry Diamond 2017 All Rights Reserved.
+*
+* Use of this source code is governed by an MIT-style license that can be
+* found in the LICENSE file at https://github.com/larrydiamond/typescriptcollectionsframework/LICENSE
+*/
+Object.defineProperty(exports, "__esModule", { value: true });
+var CollectionUtils_1 = require("../src/CollectionUtils");
+describe("Test Comparator", function () {
+    it("get the string comparator", function () {
+        var x = CollectionUtils_1.CollectionUtils.getStringComparator();
+        expect(x.compare("x", "X") == 0);
+    });
+});
