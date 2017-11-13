04d46236a22cbdd05911dc7f1b4224061d4a266d
Larry Diamond <ldiamond@ldiamond.com>
2017-11-13 15:15:09 -0500

    Testing Queue

---
+++ dist/spec/TestQueue.spec.js
@@ -0,0 +1,43 @@
+"use strict";
+/**
+* @license
+* Copyright Larry Diamond 2017 All Rights Reserved.
+*
+* Use of this source code is governed by an MIT-style license that can be
+* found in the LICENSE file at https://github.com/larrydiamond/typescriptcollectionsframework/LICENSE
+*/
+Object.defineProperty(exports, "__esModule", { value: true });
+var AllFieldCollectable_1 = require("../src/AllFieldCollectable");
+var ArrayList_1 = require("../src/ArrayList");
+var jasts_1 = require("jasts");
+describe("Test Queue functionality", function () {
+    it("Test empty queues", function () {
+        var al = new ArrayList_1.ArrayList(new AllFieldCollectable_1.AllFieldCollectable());
+        var ll = new ArrayList_1.ArrayList(new AllFieldCollectable_1.AllFieldCollectable());
+        testEmptyQueue(al, "ArrayList");
+        testEmptyQueue(ll, "LinkedList");
+    });
+    it("Test adding items to queues", function () {
+        var al = new ArrayList_1.ArrayList(new AllFieldCollectable_1.AllFieldCollectable());
+        var ll = new ArrayList_1.ArrayList(new AllFieldCollectable_1.AllFieldCollectable());
+        testAddToQueue(al, "ArrayList");
+        testAddToQueue(ll, "LinkedList");
+    });
+});
+function testEmptyQueue(queue, prefix) {
+    jasts_1.TestString.null(prefix + " peek returns null for empty queue", queue.peek());
+    jasts_1.TestString.null(prefix + " poll returns null for empty queue", queue.poll());
+    jasts_1.TestString.undefined(prefix + " removeQueue returns undefined for empty queue", queue.removeQueue());
+    jasts_1.TestString.undefined(prefix + " element returns undefined for empty queue", queue.element());
+    jasts_1.TestString.null(prefix + " second peek returns null for empty queue", queue.peek());
+    jasts_1.TestString.null(prefix + " second poll returns null for empty queue", queue.poll());
+    jasts_1.TestString.undefined(prefix + " second removeQueue returns undefined for empty queue", queue.removeQueue());
+    jasts_1.TestString.undefined(prefix + " second element returns undefined for empty queue", queue.element());
+}
+function testAddToQueue(queue, prefix) {
+    jasts_1.TestBoolean.true(prefix + " add should return true", queue.add("first"));
+    jasts_1.TestString.equals(prefix + " peek returns first for queue containing only first", queue.peek(), "first");
+    jasts_1.TestString.equals(prefix + " element returns first for queue containing only first", queue.element(), "first");
+    jasts_1.TestString.equals(prefix + " second peek returns first for queue containing only first", queue.peek(), "first");
+    jasts_1.TestString.equals(prefix + " second element returns first for queue containing only first", queue.element(), "first");
+}
---
+++ spec/TestQueue.spec.ts
@@ -0,0 +1,52 @@
+/**
+* @license
+* Copyright Larry Diamond 2017 All Rights Reserved.
+*
+* Use of this source code is governed by an MIT-style license that can be
+* found in the LICENSE file at https://github.com/larrydiamond/typescriptcollectionsframework/LICENSE
+*/
+
+import {AllFieldCollectable} from "../src/AllFieldCollectable";
+import {ArrayList} from "../src/ArrayList";
+import {LinkedList} from "../src/LinkedList";
+import {Queue} from "../src/Queue";
+import {TestBoolean, TestString} from "jasts";
+
+describe("Test Queue functionality", function() {
+
+  it("Test empty queues", function() {
+    const al:ArrayList<string> = new ArrayList<string> (new AllFieldCollectable());
+    const ll:ArrayList<string> = new ArrayList<string> (new AllFieldCollectable());
+
+    testEmptyQueue(al, "ArrayList");
+    testEmptyQueue(ll, "LinkedList");
+  });
+
+  it("Test adding items to queues", function() {
+    const al:ArrayList<string> = new ArrayList<string> (new AllFieldCollectable());
+    const ll:ArrayList<string> = new ArrayList<string> (new AllFieldCollectable());
+
+    testAddToQueue(al, "ArrayList");
+    testAddToQueue(ll, "LinkedList");
+  });
+
+});
+
+function testEmptyQueue (queue:Queue<string>, prefix:string) : void {
+  TestString.null (prefix + " peek returns null for empty queue", queue.peek());
+  TestString.null (prefix + " poll returns null for empty queue", queue.poll());
+  TestString.undefined (prefix + " removeQueue returns undefined for empty queue", queue.removeQueue());
+  TestString.undefined (prefix + " element returns undefined for empty queue", queue.element());
+  TestString.null (prefix + " second peek returns null for empty queue", queue.peek());
+  TestString.null (prefix + " second poll returns null for empty queue", queue.poll());
+  TestString.undefined (prefix + " second removeQueue returns undefined for empty queue", queue.removeQueue());
+  TestString.undefined (prefix + " second element returns undefined for empty queue", queue.element());
+}
+
+function testAddToQueue (queue:Queue<string>, prefix:string) : void {
+  TestBoolean.true (prefix + " add should return true", queue.add ("first"));
+  TestString.equals (prefix + " peek returns first for queue containing only first", queue.peek(), "first");
+  TestString.equals (prefix + " element returns first for queue containing only first", queue.element(), "first");
+  TestString.equals (prefix + " second peek returns first for queue containing only first", queue.peek(), "first");
+  TestString.equals (prefix + " second element returns first for queue containing only first", queue.element(), "first");
+}
