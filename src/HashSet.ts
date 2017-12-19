a66dc16ce835713ac3391e784720f13505b8455d
Larry Diamond <ldiamond@ldiamond.com>
2017-12-18 19:22:14 -0500

    hashset documentation

--- src/HashSet.ts
+++ src/HashSet.ts
@@ -17,6 +17,18 @@ import {ImmutableCollection} from "./ImmutableCollection";
 import {ImmutableSet} from "./ImmutableSet";
 import {JSet} from "./JSet";
 
+/**
+ * This class implements the Set interface, backed by a HashMap instance.
+ *
+ * It makes no guarantees as to the iteration order of the set; in particular, it does not guarantee that the order will remain constant over time.
+ * This class permits the null element.
+ *
+ * This class offers constant time performance for the basic operations (add, remove, contains and size),
+ * assuming the hash function disperses the elements properly among the buckets. <br>
+ * Iterating over this set requires time proportional to the sum of the HashSet instance's size
+ * (the number of elements) plus the "capacity" of the backing HashMap instance (the number of buckets). <br>
+ * Thus, it's very important not to set the initial capacity too high (or the load factor too low) if iteration performance is important.
+ */
 export class HashSet<K> implements JSet<K> {
 
   private datastore:HashMap<K,number> = null;
