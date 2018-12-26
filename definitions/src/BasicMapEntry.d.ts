950e3aacdf9b917dc39cfb50424de07a842cb5b1
Larry Diamond <ldiamond@ldiamond.com>
2018-12-26 18:31:50 -0500

    updating typedocs for BasicMapEntry

--- definitions/src/BasicMapEntry.d.ts
+++ definitions/src/BasicMapEntry.d.ts
@@ -7,7 +7,17 @@
 */
 import { MapEntry } from "./MapEntry";
 /**
- * BasicMapEntry is immutable once constructed.   Mutability is provided via the Map implementation
+ * A map entry (key-value pair).
+ *
+ * The Map.entrySet method returns a collection-view of the map, whose elements are of this class.
+ *
+ * The only way to obtain a reference to a map entry is from the iterator of this collection-view.
+ *
+ * These MapEntry objects are valid only for the duration of the iteration; more formally,
+ * the behavior of a map entry is undefined if the backing map has been modified after the entry
+ * was returned by the iterator, except through the setValue operation on the map entry.
+ *
+ * BasicMapEntry is immutable once constructed.   Mutability is provided via the Map implementation.
  */
 export declare class BasicMapEntry<K, V> implements MapEntry<K, V> {
     protected key: K;
@@ -15,11 +25,16 @@ export declare class BasicMapEntry<K, V> implements MapEntry<K, V> {
     constructor(iKey: K, iValue: V);
     /**
      * Returns the key corresponding to this entry.
+     *
      * @return {K} the key corresponding to this entry
      */
     getKey(): K;
     /**
-     * Returns the value corresponding to this entry. If the mapping has been removed from the backing map (by the iterator's remove operation), the results of this call are undefined.
+     * Returns the value corresponding to this entry.
+     *
+     * If the mapping has been removed from the backing map (by the iterator's remove operation),
+     * the results of this call are undefined.
+     *
      * @return {V} the value corresponding to this entry
      */
     getValue(): V;
--- dist/src/BasicMapEntry.js
+++ dist/src/BasicMapEntry.js
@@ -8,7 +8,17 @@
 */
 Object.defineProperty(exports, "__esModule", { value: true });
 /**
- * BasicMapEntry is immutable once constructed.   Mutability is provided via the Map implementation
+ * A map entry (key-value pair).
+ *
+ * The Map.entrySet method returns a collection-view of the map, whose elements are of this class.
+ *
+ * The only way to obtain a reference to a map entry is from the iterator of this collection-view.
+ *
+ * These MapEntry objects are valid only for the duration of the iteration; more formally,
+ * the behavior of a map entry is undefined if the backing map has been modified after the entry
+ * was returned by the iterator, except through the setValue operation on the map entry.
+ *
+ * BasicMapEntry is immutable once constructed.   Mutability is provided via the Map implementation.
  */
 var BasicMapEntry = /** @class */ (function () {
     function BasicMapEntry(iKey, iValue) {
@@ -17,13 +27,18 @@ var BasicMapEntry = /** @class */ (function () {
     }
     /**
      * Returns the key corresponding to this entry.
+     *
      * @return {K} the key corresponding to this entry
      */
     BasicMapEntry.prototype.getKey = function () {
         return this.key;
     };
     /**
-     * Returns the value corresponding to this entry. If the mapping has been removed from the backing map (by the iterator's remove operation), the results of this call are undefined.
+     * Returns the value corresponding to this entry.
+     *
+     * If the mapping has been removed from the backing map (by the iterator's remove operation),
+     * the results of this call are undefined.
+     *
      * @return {V} the value corresponding to this entry
      */
     BasicMapEntry.prototype.getValue = function () {
