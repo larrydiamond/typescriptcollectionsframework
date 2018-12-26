c9ecde067065e9149ac0d4e33519e2ab4958c524
Larry Diamond <ldiamond@ldiamond.com>
2018-12-26 18:43:30 -0500

    Typedoc improvements for Consumer interface

--- definitions/src/Consumer.d.ts
+++ definitions/src/Consumer.d.ts
@@ -5,6 +5,13 @@
 * Use of this source code is governed by an MIT-style license that can be
 * found in the LICENSE file at https://github.com/larrydiamond/typescriptcollectionsframework/blob/master/LICENSE
 */
+/**
+ * Represents an operation that accepts a single input argument and returns no result.
+ *
+ * Unlike most other functional interfaces, Consumer is expected to operate via side-effects.
+ *
+ * This is a functional interface whose functional method is accept(t:T).
+ */
 export interface Consumer<T> {
     /**
      * Performs this operation on the given argument
--- src/Consumer.ts
+++ src/Consumer.ts
@@ -6,6 +6,13 @@
 * found in the LICENSE file at https://github.com/larrydiamond/typescriptcollectionsframework/blob/master/LICENSE
 */
 
+/**
+ * Represents an operation that accepts a single input argument and returns no result. 
+ * 
+ * Unlike most other functional interfaces, Consumer is expected to operate via side-effects.
+ * 
+ * This is a functional interface whose functional method is accept(t:T).
+ */
 export interface Consumer<T> {
   /**
    * Performs this operation on the given argument
